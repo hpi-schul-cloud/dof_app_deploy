e0319b4df0d6a47b6772ff0bafff6f763852aaa5
Phillip Wirth <phillip.wirth@dataport.de>
2022-12-29 10:56:13 +0100

    BC-2785 added unit test

---
+++ tests/test.py
@@ -0,0 +1,22 @@
+import unittest
+import yaml
+import os
+
+PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
+
+
+class TestEverything(unittest.TestCase):
+
+    def test_group_vars_all_config(self):
+        config_yml_file = os.path.join(PROJECT_DIR, 'ansible', 'group_vars', 'all', 'config.yml')
+        with open(config_yml_file, 'r') as file:
+            config_all = yaml.safe_load(file)
+            useless_declarations = []
+            for entry in config_all['configuration_all']:
+                res = not config_all['configuration_all'][entry]['server'] \
+                      and not config_all['configuration_all'][entry]['client'] \
+                      and not config_all['configuration_all'][entry]['nuxtclient']
+                if res:
+                    useless_declarations.append(entry)
+
+            self.assertEqual([], useless_declarations, 'populating a variable that is never set anywhere')
